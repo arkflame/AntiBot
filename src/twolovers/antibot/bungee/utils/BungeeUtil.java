bdd2c7070bf6c57c2469eb335c68c60199ed4c31
kxmpetentes <mikkakohnz@gmail.com>
2021-04-27 02:28:17 +0200

    little changes

--- src/twolovers/antibot/bungee/utils/BungeeUtil.java
+++ src/twolovers/antibot/bungee/utils/BungeeUtil.java
@@ -1,17 +1,16 @@
 package twolovers.antibot.bungee.utils;
 
-import java.util.Locale;
-
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.Locale;
 
 public class BungeeUtil {
     public static String getLanguage(final ProxiedPlayer proxiedPlayer, final String defaultString) {
         final Locale locale = proxiedPlayer.getLocale();
 
-        if (locale == null) {
-            return defaultString;
-        } else {
-            return locale.toLanguageTag();
-        }
+        if (locale != null) return locale.toLanguageTag();
+
+        return defaultString;
+
     }
 }
--- src/twolovers/antibot/bungee/utils/ConfigUtil.java
+++ src/twolovers/antibot/bungee/utils/ConfigUtil.java
@@ -1,91 +1,89 @@
 package twolovers.antibot.bungee.utils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.util.logging.Logger;
-
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.scheduler.TaskScheduler;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.logging.Logger;
+
 public class ConfigUtil {
-	private final Plugin plugin;
-	private final Logger logger;
-	private final TaskScheduler scheduler;
+    private final Plugin plugin;
+    private final Logger logger;
+    private final TaskScheduler scheduler;
 
-	public ConfigUtil(final Plugin plugin) {
-		this.plugin = plugin;
-		this.logger = plugin.getLogger();
-		this.scheduler = plugin.getProxy().getScheduler();
-	}
+    public ConfigUtil(final Plugin plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.getLogger();
+        this.scheduler = plugin.getProxy().getScheduler();
+    }
 
-	public Configuration getConfiguration(String file) {
-		file = replaceDataFolder(file);
+    public Configuration getConfiguration(String file) {
+        file = replaceDataFolder(file);
 
-		try {
-			return ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(file));
-		} catch (IOException e) {
-			return new Configuration();
-		}
-	}
+        try {
+            return ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(file));
+        } catch (IOException e) {
+            return new Configuration();
+        }
+    }
 
-	public void createConfiguration(String file) {
-		try {
-			file = replaceDataFolder(file);
+    public void createConfiguration(String file) {
+        try {
+            file = replaceDataFolder(file);
 
-			final File configFile = new File(file);
+            final File configFile = new File(file);
 
-			if (!configFile.exists()) {
-				final String[] files = file.split("/");
-				final InputStream inputStream = plugin.getClass().getClassLoader()
-						.getResourceAsStream(files[files.length - 1]);
-				final File parentFile = configFile.getParentFile();
+            if (configFile.exists()) return;
 
-				if (parentFile != null)
-					parentFile.mkdirs();
+            final String[] files = file.split("/");
+            final InputStream inputStream = plugin.getClass().getClassLoader().getResourceAsStream(files[files.length - 1]);
+            final File parentFile = configFile.getParentFile();
 
-				if (inputStream != null) {
-					Files.copy(inputStream, configFile.toPath());
-				} else {
-					configFile.createNewFile();
-				}
+            if (parentFile != null) parentFile.mkdirs();
 
-				logger.info("File " + configFile + " has been created!");
-			}
-		} catch (final IOException e) {
-			logger.info("Unable to create configuration file '" + file + "'!");
-		}
-	}
+            if (inputStream != null) {
+                Files.copy(inputStream, configFile.toPath());
+            } else {
+                configFile.createNewFile();
+            }
 
-	public void saveConfiguration(final Configuration configuration, final String file) {
-		final String replacedFile = replaceDataFolder(file);
+            logger.info("File " + configFile + " has been created!");
+        } catch (final IOException e) {
+            logger.info("Unable to create configuration file '" + file + "'!");
+        }
+    }
 
-		this.scheduler.runAsync(plugin, () -> {
-			try {
-				ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(replacedFile));
-			} catch (final IOException e) {
-				logger.info("Unable to save configuration file '" + replacedFile + "'!");
-			}
-		});
-	}
+    public void saveConfiguration(final Configuration configuration, final String file) {
+        final String replacedFile = replaceDataFolder(file);
 
-	public void deleteConfiguration(final String file) {
-		final String replacedFile = replaceDataFolder(file);
-		final File file1 = new File(replacedFile);
+        this.scheduler.runAsync(plugin, () -> {
+            try {
+                ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(replacedFile));
+            } catch (final IOException e) {
+                logger.info("Unable to save configuration file '" + replacedFile + "'!");
+            }
+        });
+    }
 
-		if (file1.exists()) {
-			file1.delete();
-			logger.info("File " + replacedFile + " has been deleted!");
-		}
-	}
+    public void deleteConfiguration(final String file) {
+        final String replacedFile = replaceDataFolder(file);
+        final File file1 = new File(replacedFile);
 
-	private String replaceDataFolder(final String string) {
-		final File dataFolder = plugin.getDataFolder();
+        if (file1.exists()) {
+            file1.delete();
+            logger.info("File " + replacedFile + " has been deleted!");
+        }
+    }
 
-		return string.replace("%datafolder%", dataFolder.toPath().toString());
-	}
+    private String replaceDataFolder(final String string) {
+        final File dataFolder = plugin.getDataFolder();
+
+        return string.replace("%datafolder%", dataFolder.toPath().toString());
+    }
 }
